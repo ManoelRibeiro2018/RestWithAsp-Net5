4c99c9d9cbef880f5b771771ce5f3567f28049f1
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-08-13 12:04:17 -0300

    Implementando Pattern Specification

---
+++ RestWithAsp-core5/RestWithAsp-core5/Specification/Implementation/BookSpecification.cs
@@ -0,0 +1,25 @@
+﻿using RestWithAsp_core5.Model;
+using RestWithAsp_core5.Repository;
+using RestWithAsp_core5.Specification.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_core5.Especific
+{
+    public class BookSpecification : ISpecification<Book>
+    {
+        private readonly IGenericRepository<Book> _bookRepository;
+
+        public BookSpecification(IGenericRepository<Book> bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public bool IsSatisfiedBy(Book Entity)
+        {
+            return _bookRepository.FindById(Entity.Id) != null;
+        }
+    }
+}
---
+++ RestWithAsp-core5/RestWithAsp-core5/Specification/Interface/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_core5.Specification.Interface
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T Entity);
+    }
+}
